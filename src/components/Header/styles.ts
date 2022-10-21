7bad7805f534a785f2b26d05fcc006347168de8f
Misael Lopes <mecl.ely@gmail.com>
2022-10-21 09:31:13 +0100

    Initial commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    nav {
+        
+    }
+`
