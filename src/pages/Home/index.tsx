0380133bf9c12c5ac3dbf16c4546a278929bfb66
Misael Lopes <mecl.ely@gmail.com>
2022-10-24 16:26:15 +0100

    HomeContainer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { HomeContainer } from "./styles";
 import bannerIMG from './../../assets/banner.png'
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
 
 export function Home() {
 
@@ -10,10 +11,22 @@ export function Home() {
                     <h1>Encontre o café perfeito para qualquer hora do dia</h1>
                     <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
                     <ul>
-                        <li>Compra simples e segura</li>
-                        <li>Embalagem mantém o café intacto</li>
-                        <li>Entrega rápida e rastreada</li>
-                        <li>O café chega fresquinho até você</li>
+                        <li>
+                            <span><ShoppingCart size={16} weight='fill' /></span>
+                            Compra simples e segura
+                        </li>
+                        <li>
+                            <span><Package size={16} weight='fill' /></span>
+                            Embalagem mantém o café intacto
+                        </li>
+                        <li>
+                            <span><Timer size={16} weight='fill' /></span>
+                            Entrega rápida e rastreada
+                        </li>
+                        <li>
+                            <span><Coffee size={16} weight='fill' /></span>
+                            O café chega fresquinho até você
+                        </li>
                     </ul>
                 </div>
                 <img src={bannerIMG} alt="Banner - Imagem de Copo de Coffee" />
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,4 +4,88 @@ import styled from "styled-components";
 export const HomeContainer = styled.main`
     width: 100%;
     height: 100%;
+
+    .banner{
+        padding: 5.875rem 0;
+        display: flex;
+        align-items: flex-start;
+        justify-content: flex-start;
+        gap: 0.6875rem;
+
+        .content {
+            width: 55%;
+            display: flex;
+            flex-direction: column;
+
+            h1 {
+                font-family: 'Baloo';
+                font-weight: 800;
+                font-size: 3rem;
+                line-height: 1.3;
+                color: ${props => props.theme['base-title']};
+            }
+
+            p {
+                margin-top: 1rem;
+                margin-bottom: 4.125rem;
+                flex: none;
+                flex-grow: 0;
+                align-self: stretch;
+                font-size: 1.25rem;
+                padding-right: 2rem;
+                color: ${props => props.theme['base-subtitle']};
+            }
+
+            ul {
+                list-style: none;
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                grid-template-rows: 1fr 1fr;
+                row-gap: 1.25rem;
+                
+
+                li {
+                    display: flex;
+                    align-items: center;
+                    justify-content: flex-start;
+                    gap: 0.5rem;
+                    color: ${props => props.theme['base-text']};
+                }
+
+                li:nth-child(1) span{
+                    background: ${props => props.theme['yellow-dark']};
+                }
+                li:nth-child(2) span{
+                    background: ${props => props.theme['base-text']};
+                }
+                li:nth-child(3) span{
+                    background: ${props => props.theme['yellow']};
+                }
+                li:nth-child(4) span{
+                    background: ${props => props.theme['purple']};
+                }
+
+                span {
+                    width: 2rem;
+                    height: 2rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    border-radius: 50px;
+
+                    svg {
+                        color: ${props => props.theme['white']};
+                    }
+
+                }
+
+                
+    
+            }
+        }
+
+        img {
+            width: 40%;
+        }
+    }
 `
